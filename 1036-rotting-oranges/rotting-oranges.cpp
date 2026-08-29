be7a08695d460c6ba351b8612e81f8e5fbbb508b
niranjanravi24 <niranjanravi0924@gmail.com>
2026-08-29 12:22:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (16.62%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,48 +1,45 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m =grid.size();
+        int m = grid.size();
         int n = grid[0].size();
         queue<pair<pair<int,int>,int>> q;
         vector<vector<int>> vis(m, vector<int>(n,0));
         for(int i=0; i<m; i++){
-
             for(int j=0; j<n; j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
-                    vis[i][j] = 2;
+                    vis[i][j]=2;
                 }
             }
         }
         vector<int> delRow = {-1,0,1,0};
         vector<int> delCol = {0,1,0,-1};
-        int tm = 0;
+        int time = 0;
         while(!q.empty()){
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
-            tm = max(t,tm);
+            time = max(t,time);
             q.pop();
-
             for(int i=0; i<4; i++){
-                int nRow = r + delRow[i];
-                int nCol = c + delCol[i];
-                if(nRow>=0 && nRow<m && nCol>=0 && nCol<n &&
-                vis[nRow][nCol]!=2 && grid[nRow][nCol]==1 ){
-                    q.push({{nRow, nCol}, t+1});
+                int nRow = delRow[i]+r;
+                int nCol = delCol[i]+c;
+                if(nRow>=0 && nRow<m && nCol>=0 && nCol<n && 
+                vis[nRow][nCol]!=2 && grid[nRow][nCol]==1){
+                    q.push({{nRow,nCol},t+1});
                     vis[nRow][nCol] = 2;
                 }
             }
-            
         }
 
-        for(int i = 0; i<m; i++){
+        for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                if(vis[i][j]!=2 && grid[i][j]==1){
+                if(grid[i][j]==1 && vis[i][j]!=2){
                     return -1;
                 }
             }
         }
-        return tm;
+        return time;
     }
 };
