a69f71bc06564e4001cc1366953fd9aa29cacf9f
niranjanravi24 <niranjanravi0924@gmail.com>
2026-09-11 22:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (57.84%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-         if(nums.size()==1) return true;
-        int near = 0;
-        int far = 0;
-        while(far<nums.size()-1){
-            int farthest = 0;
-            for(int i=near; i<=far; i++){
-                farthest = max(farthest, nums[i]+i);
+        int maxReach = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(i>maxReach){
+                return false;
             }
-            near = far+1;
-            far = farthest;
-            if(!far) return false;
+            maxReach = max(maxReach,i+nums[i]);
         }
         return true;
+    
     }
 };
